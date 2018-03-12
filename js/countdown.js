d68940c5781e65c66ad79e08a29a7db280e5fae8
Alexander Melo <alexandermelox@gmail.com>
2018-03-12 19:29:34 -0400

    fixed safari time bug

--- js/countdown.js
+++ js/countdown.js
@@ -1,7 +1,9 @@
 const countdown = document.querySelector('.countdown__timer');
 
 // Set Launch Date (ms)
-const launchDate = new Date('Oct 12, 2018 24:00:00').getTime();
+// const launchDate = new Date('Oct 12, 2018 24:00:00').getTime();
+const launchDate = new Date('2018-10-12T24:00:00'.replace(/\s/, 'T')).getTime();
+
 
 // Update every second
 const intvl = setInterval(() => {
